be80183026c5f4650ee0502804c190ca83c8e659
74789 <747895312@qq.com>
2022-05-23 14:01:28 +0800

    hot-fix commit

--- src/main/java/com/aaa/git/GitTest.java
+++ src/main/java/com/aaa/git/GitTest.java
@@ -3,5 +3,6 @@ package com.aaa.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
+        System.out.println("Hello,Git2!");
     }
 }
